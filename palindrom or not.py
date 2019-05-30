24836f372098a5448a42cdeef570e91ccada395c
divyarthik <51078638+divyarthik@users.noreply.github.com>
2019-05-30 11:10:41 +0530

    Create palindrom or not.py

---
+++ palindrom or not.py
@@ -0,0 +1,11 @@
+def palindrome(str):
+for i in range(0,len(str//2)
+  if str[i]!=str[len(stri)-i-1]
+   return false
+return true
+s=malai
+ans=palindrome(s)
+if (ans):
+   print("yes")
+else:
+   print("no")
