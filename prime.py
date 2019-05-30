c3ac6785e8ffe24aea18f311af50106470cb239f
divyarthik <51078638+divyarthik@users.noreply.github.com>
2019-05-30 11:31:09 +0530

    Create prime.py

---
+++ prime.py
@@ -0,0 +1,11 @@
+num=12
+if num>1:
+for i in range(0,5//2)
+  if(num % i==0)
+      print(num,"is not a prime number")
+      break:
+   else:
+      print(num,"is a prime number")
+else:
+   print(num,"is a not a prime number")
+    
